0dfef35c578e6d9c05a786bad0da8babdf057950
angela1393aa <angela1393aa@gmail.com>
2021-08-24 18:56:03 +0800

    爬蟲

---
+++ crawler/crawler1.js
@@ -0,0 +1,23 @@
+// GET
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY
+// ?
+// response=json
+// &
+// date=20210807
+// &
+// stockNo=2330
+const axios = require("axios");
+//日期
+const moment = require("moment");
+let today = moment().format("YYYYMMDD");
+axios
+  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+    params: {
+      response: "json",
+      date: today,
+      stockNo: "2330",
+    },
+  })
+  .then((response) => {
+    console.log(response.data);
+  });
